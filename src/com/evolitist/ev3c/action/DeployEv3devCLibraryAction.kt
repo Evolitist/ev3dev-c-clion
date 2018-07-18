95aae3a4d713df8135466ff23cb3e6a1176444ce
Evolitist <evolitist@gmail.com>
2018-07-18 23:20:17 +0700

    Implement automated library deployment

--- src/com/evolitist/ev3c/action/DeployEv3devCLibraryAction.kt
+++ src/com/evolitist/ev3c/action/DeployEv3devCLibraryAction.kt
@@ -1,7 +1,5 @@
 package com.evolitist.ev3c.action
 
-import com.evolitist.ev3c.run
-import com.evolitist.ev3c.translateToWSL
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.intellij.openapi.actionSystem.AnAction
@@ -11,17 +9,19 @@ import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.wm.WindowManager
+import com.intellij.ssh.ConnectionBuilder
 import com.intellij.ui.JBColor
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.util.io.HttpRequests
 import java.io.File
+import java.util.concurrent.TimeUnit
 
-class InstallCLibraryAction : AnAction() {
+class DeployEv3devCLibraryAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         //TODO: launch some sort of a "library selection wizard"
-        runBackgroundableTask("Installing library...", event.project, true) {
+        runBackgroundableTask("Deploying library...", event.project, true) {
             //TODO: check if libraries are already installed
-            val json = HttpRequests.request(RELEASE_URL)
+            val json = HttpRequests.request(InstallCLibraryAction.RELEASE_URL)
                     .connectTimeout(0)
                     .readTimeout(0)
                     .redirectLimit(10)
@@ -33,11 +33,11 @@ class InstallCLibraryAction : AnAction() {
             val downloadUrl = data.getAsJsonArray("assets")[0].asJsonObject
                     .get("browser_download_url").asString
             val tempFile: File
-            if (lastId < currentId || lastTempFile == null) {
-                lastId = currentId
+            if (InstallCLibraryAction.lastId < currentId || InstallCLibraryAction.lastTempFile == null) {
+                InstallCLibraryAction.lastId = currentId
                 tempFile = File.createTempFile("ev3dev-c-release", ".zip")
                 tempFile.deleteOnExit()
-                lastTempFile = tempFile.absolutePath
+                InstallCLibraryAction.lastTempFile = tempFile.absolutePath
                 HttpRequests.request(downloadUrl)
                         .connectTimeout(0)
                         .readTimeout(0)
@@ -45,41 +45,42 @@ class InstallCLibraryAction : AnAction() {
                         .saveToFile(tempFile, it)
                 if (it.isCanceled) return@runBackgroundableTask
             } else {
-                tempFile = File(lastTempFile)
+                tempFile = File(InstallCLibraryAction.lastTempFile)
             }
 
-            "unzip ${tempFile.translateToWSL()}".run(it)
-            if (it.isCanceled) {
-                "rm -rf lib/ include/".run()
-                return@runBackgroundableTask
-            }
-            "cp -f lib/* /usr/local/lib/".run(it)
-            if (it.isCanceled) {
-                "rm -rf lib/ include/".run()
-                return@runBackgroundableTask
-            }
-            "cp -f include/* /usr/local/include/".run(it)
-            if (it.isCanceled) {
-                "rm -rf lib/ include/".run()
-                return@runBackgroundableTask
-            }
-            "sudo ldconfig".run(it)
-            "rm -rf lib/ include/".run(it)
+            val connBuilder = ConnectionBuilder("192.168.0.1", 22)
+                    .withUsername("robot")
+                    .withPassword("maker")
+            if (it.isCanceled) return@runBackgroundableTask
+            connBuilder.openSftpChannel().uploadFileOrDir(tempFile, "/home/robot/", "release.zip")
+            if (it.isCanceled) return@runBackgroundableTask
+            connBuilder.execBuilder("unzip release.zip")
+                    .execute()
+                    .waitFor(10, TimeUnit.SECONDS)
+            if (it.isCanceled) return@runBackgroundableTask
+            connBuilder.execBuilder("echo maker | sudo -S cp -f lib/* /usr/local/lib/")
+                    .execute()
+                    .waitFor(10, TimeUnit.SECONDS)
+            if (it.isCanceled) return@runBackgroundableTask
+            connBuilder.execBuilder("echo maker | sudo -S cp -f include/* /usr/local/include/")
+                    .execute()
+                    .waitFor(10, TimeUnit.SECONDS)
+            if (it.isCanceled) return@runBackgroundableTask
+            connBuilder.execBuilder("echo maker | sudo -S ldconfig")
+                    .execute()
+                    .waitFor(10, TimeUnit.SECONDS)
+            connBuilder.execBuilder("rm -rf release.zip include/ lib/")
+                    .execute()
+                    .waitFor(10, TimeUnit.SECONDS)
             val statusBar = WindowManager.getInstance().getStatusBar(event.project)
             ApplicationManager.getApplication().invokeLater {
                 JBPopupFactory.getInstance().createHtmlTextBalloonBuilder(
-                        "Library installed!", null, JBColor(12250810, 3359022), null
+                        "Library deployed!", null, JBColor(12250810, 3359022), null
                 ).createBalloon().show(
                         RelativePoint.getCenterOf(statusBar.component),
                         Balloon.Position.above
                 )
             }
         }
-    }
-
-    companion object {
-        const val RELEASE_URL = "https://api.github.com/repos/Evolitist/ev3dev-c/releases/latest"
-        var lastId = 0
-        var lastTempFile: String? = null
     }
 }
