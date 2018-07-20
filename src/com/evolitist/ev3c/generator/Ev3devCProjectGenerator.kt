8a2db4ee5799dd7733a3f98910bc60d6f931f6c8
Evolitist <evolitist@gmail.com>
2018-07-20 19:57:13 +0700

    Stabilize current plugin version

--- src/com/evolitist/ev3c/generator/Ev3devCProjectGenerator.kt
+++ src/com/evolitist/ev3c/generator/Ev3devCProjectGenerator.kt
@@ -2,21 +2,16 @@ package com.evolitist.ev3c.generator
 
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.cidr.cpp.cmake.projectWizard.generators.CMakeProjectGenerator
-import com.jetbrains.cidr.toolchains.OSType
-import javax.swing.*
+import javax.swing.Box
+import javax.swing.BoxLayout
+import javax.swing.JLabel
+import javax.swing.JPanel
 
 class Ev3devCProjectGenerator : CMakeProjectGenerator() {
-    private val ipByOS: String
-        get() = when(OSType.getCurrent()) {
-            OSType.WIN -> "192.168.137.3"
-            OSType.LINUX -> "10.42.0.3"
-            OSType.MAC -> "ev3dev" //TODO: find a Mac to test
-        }
-
     override fun getName() = "ev3dev C program"
 
     override fun createSourceFiles(projectName: String, projectRootDir: VirtualFile): Array<VirtualFile> = arrayOf(
-            createProjectFileWithContent(projectRootDir, "main.c", "#include \"rbc.h\"\n\ntask main()\n{\n\n}\n")
+            createProjectFileWithContent(projectRootDir, "main.c", "#include \"rbc.h\"\n\ntask main() {\n\n}\n")
     )
 
     override fun getCMakeFileContent(projectName: String) = "cmake_minimum_required(VERSION 3.5.1)\n" +
@@ -26,12 +21,12 @@ class Ev3devCProjectGenerator : CMakeProjectGenerator() {
             "link_directories(/usr/local/lib)\n" +
             "\n" +
             "set(CMAKE_C_STANDARD 99)\n" +
-            "set(CMAKE_VERBOSE_MAKEFILE ON)\n" +
+            "set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)\n" +
             "set(CMAKE_C_LINK_EXECUTABLE \"arm-linux-gnueabi-gcc <LINK_LIBRARIES> <OBJECTS> -o <TARGET> -lev3dev-c -lpthread -lm\")\n" +
             "\n" +
             "add_executable($projectName main.c)\n" +
             "\n" +
-            "add_custom_command(TARGET $projectName POST_BUILD COMMAND sshpass -p 'maker' scp $projectName robot@$ipByOS:~)"
+            "add_custom_command(TARGET $projectName POST_BUILD COMMAND sshpass -p 'maker' scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $projectName robot@192.168.0.1:~)"
 
     override fun getSettingsPanel() = SettingsPanel()
 
